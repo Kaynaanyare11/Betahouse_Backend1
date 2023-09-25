6e05ea1e7f542ba452d8e42fe359df07cefc888a
kaynaanyare11 <khudari.januune.xassan@gmail.com>
2023-09-25 12:54:41 +0300

    i removed authentication from houses get all

--- routes/houses.js
+++ routes/houses.js
@@ -3,7 +3,7 @@ const router = express.Router()
 const housesControllers = require('../controller/HouseController')
 const AuthenticateRoute = require('./AuthenticationMiddleware')
 /* GET home page. */
-router.get('/',AuthenticateRoute(["Admin","SuperAdmin","CustomerCare"]), housesControllers.getHouses)
+router.get('/',housesControllers.getHouses)
 router.post('/',AuthenticateRoute(["Admin","SuperAdmin"]), housesControllers.createHouse)
 router.put('/:id',AuthenticateRoute(["Admin","SuperAdmin"]), housesControllers.PutHouse)
 router.delete('/:id',AuthenticateRoute(["Admin","SuperAdmin"]), housesControllers.DeleteHouse)
