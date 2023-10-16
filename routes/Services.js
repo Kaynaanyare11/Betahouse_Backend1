fa3586461e75fbcd1c3c2aeb4ab833d7040f1a5a
kaynaanyare11 <khudari.januune.xassan@gmail.com>
2023-10-16 15:31:03 +0300

    removed post permission

--- routes/Services.js
+++ routes/Services.js
@@ -4,7 +4,7 @@ const ServiceControllers = require('../controller/ServicesControlller');
 const AuthenticateRoute = require('./AuthenticationMiddleware')
 /* GET home page. */
 router.get('/',ServiceControllers.getService);
-router.post('/',AuthenticateRoute(["Admin","SuperAdmin","CustomerCare"]),ServiceControllers.createService);
+router.post('/',ServiceControllers.createService);
 router.get('/:id',AuthenticateRoute(["Admin","SuperAdmin","CustomerCare"]),ServiceControllers.getServiceById);
 router.put('/:id',AuthenticateRoute(["Admin","SuperAdmin","CustomerCare"]),ServiceControllers.PutService);
 router.delete('/:id',AuthenticateRoute(["Admin","SuperAdmin"]),ServiceControllers.DeleteService);
